8d1a4d1dac7c6b3f864a807ab54531925844563d
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-06-02 15:24:34 +0330

    repo updated

---
+++ JS Course/Working With Arrays/3-Simple Array Methods/main.js
@@ -0,0 +1,10 @@
+// slice method
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.slice(0, 2));
+console.log(arr.slice(-1));
+console.log(arr.slice(-2));
+console.log(arr.slice(2));
+console.log(arr);
+
+// splice method
