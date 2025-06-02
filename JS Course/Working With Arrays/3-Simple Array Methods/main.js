6627eebbdd85f9a5b0b50d33c5e68b522f79fee6
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-06-02 15:54:57 +0330

    repo updated

--- JS Course/Working With Arrays/3-Simple Array Methods/main.js
+++ JS Course/Working With Arrays/3-Simple Array Methods/main.js
@@ -1,16 +1,27 @@
-// slice method
-
+// // slice method
 const arr = [1, 2, 3, 4, 5];
-// console.log(arr.slice(0, 2));
-// console.log(arr.slice(-1));
-// console.log(arr.slice(-2));
-// console.log(arr.slice(2));
+// // console.log(arr.slice(0, 2));
+// // console.log(arr.slice(-1));
+// // console.log(arr.slice(-2));
+// // console.log(arr.slice(2));
+// // console.log(arr);
+
+// // splice method
+// // console.log(arr.splice(1, 3));
+// console.log(arr.splice(-2));
+// // console.log(arr);
+// // console.log(arr.splice(3));
+// // console.log(arr.slice(-2));
 // console.log(arr);
 
-// splice method
-// console.log(arr.splice(1, 3));
-console.log(arr.splice(-2));
-// console.log(arr);
-// console.log(arr.splice(3));
-// console.log(arr.slice(-2));
-console.log(arr);
+// reverse method
+// console.log(arr.reverse());
+// concat method
+
+const arr2 = [6, 7];
+
+// console.log(arr.concat(arr2));
+
+// console.log([...arr, ...arr2]);
+
+// join method
