a2ad2e8ca4d7d79cadd133d3fb44370ff8bb8560
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-06-03 16:42:56 +0330

    repo updated

--- JS Course/Working With Arrays/main.js
+++ JS Course/Working With Arrays/main.js
@@ -38,7 +38,46 @@
 
 // looping array : forEach
 
-const arr = [1, 2, 3];
-arr.forEach(function (value, index, arr) {
-  console.log(value, index, arr);
+// const arr = [1, 2, 3];
+// arr.forEach(function (value, index, arr) {
+//   console.log(value, index, arr);
+// });
+
+// // /////////////
+
+// const movements = [200, -100, 300, -50];
+
+// movements.forEach(function (mov, i) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+// const arr2 = ["ali", "mohammad"];
+
+// arr2.forEach(function (value, index, arr2) {
+//   console.log(`your name is ${value} on number ${index + 1}`);
+// });
+
+// forEach with maps and sets
+
+// maps
+const map = new Map([
+  ["usd", "united states"],
+  ["eur", "euro"],
+  ["gbp", "pound sterling"],
+]);
+
+map.forEach(function (value, index, map) {
+  console.log(`${value} : ${index}`);
+});
+
+// sets
+console.log("set");
+const mySet = new Set(["eur", "eur", "usd"]);
+
+mySet.forEach(function (value, _, mySet) {
+  console.log(`${(value, _)}`);
 });
