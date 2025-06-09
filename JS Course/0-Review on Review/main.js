8c07e854792bfb301ba0bf1a72d3116abfe02991
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-06-09 11:18:17 +0330

    repo updated

---
+++ JS Course/0-Review on Review/main.js
@@ -0,0 +1,20 @@
+// working with arrays Seasson
+
+console.log(typeof Array);
+const arr = [1, 2, 3, 4];
+// console.log(typeof arr);
+// slice
+// console.log(arr.slice(-1));
+// console.log(arr.slice(-2));
+// console.log(arr.slice());
+// its like [...arr]
+// console.log([...arr]);
+
+// splice
+// console.log(arr.splice(2, 1));
+// console.log(arr.splice(1, 3));
+// console.log(arr);
+// console.log(arr.splice(-2));
+
+// reverse
+console.log(arr.reverse());
