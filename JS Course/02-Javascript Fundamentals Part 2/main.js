043cf59cf3b554b75e03caa61b0ee5db294d18d0
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-07-18 12:20:05 +0330

    repository updated

--- JS Course/02-Javascript Fundamentals Part 2/main.js
+++ JS Course/02-Javascript Fundamentals Part 2/main.js
@@ -21,3 +21,11 @@ const hello3 = () => "hello3";
 console.log(hello3());
 
 // challenge #2
+const calcTip = function (bill) {
+  let tip = 0;
+  bill > 30 && bill < 500 ? (tip = 0.15 * bill) : (tip = 0.2 * bill);
+  const tips = [];
+  const total = tip + bill;
+  return total;
+};
+console.log(calcTip(500));
