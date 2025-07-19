198cf5744d46f32f9018023b3938fbb0672000b3
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-07-19 11:36:04 +0330

    repository updated

--- JS Course/12-Numbers,Dates/main.js
+++ JS Course/12-Numbers,Dates/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// Converting and Checking Numbers
+console.log(Number("23"));
+console.log(+"25");
+console.log(Number("jonas"));
+console.log(typeof NaN);
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("23"));
+console.log(Number.isNaN(+"23x"));
+console.log(Number.isFinite(23 / 0));
+console.log(Number.isFinite(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23.8));
