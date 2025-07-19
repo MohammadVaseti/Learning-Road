ee92a1ea3e117b9aa837af37015f819ed3e3082b
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-07-19 17:17:36 +0330

    repository updated

--- JS Course/12-Numbers,Dates/main.js
+++ JS Course/12-Numbers,Dates/main.js
@@ -1,14 +1,26 @@
 "use strict";
 
 // Converting and Checking Numbers
-console.log(Number("23"));
-console.log(+"25");
-console.log(Number("jonas"));
-console.log(typeof NaN);
-console.log(Number.isNaN(20));
-console.log(Number.isNaN("23"));
-console.log(Number.isNaN(+"23x"));
-console.log(Number.isFinite(23 / 0));
-console.log(Number.isFinite(23));
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger(23.8));
+// console.log(Number("23"));
+// console.log(+"25");
+// console.log(Number("jonas"));
+// console.log(typeof NaN);
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN("23"));
+// console.log(Number.isNaN(+"23x"));
+// console.log(Number.isFinite(23 / 0));
+// console.log(Number.isFinite(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23.8));
+
+// Math and Rounding
+
+console.log(Math.sqrt(4));
+console.log(8 ** (1 / 2));
+console.log(9 ** (1 / 2));
+console.log(8 ** (1 / 3));
+console.log(Math.min(2, 7, 56));
+console.log(Math.max(2, 7, 56));
+
+const ranInt = (min, max) => Math.trunc(Math.random() * (max - min)) + min;
+console.log(ranInt(5, 25));
