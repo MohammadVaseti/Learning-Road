ae9c22e29bf67eed2b237a3854966c228fe5f5cc
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-08-04 11:48:33 +0330

    repository updated

--- JS Course/13 - Advanced DOM and Events/main.js
+++ JS Course/13 - Advanced DOM and Events/main.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// PROJECT: "Bankist" Website
+const p = document.querySelectorAll("p");
+console.log(p);
+// can use forEach on nodelists
+p.forEach((e, i, arr) => console.log(e));
