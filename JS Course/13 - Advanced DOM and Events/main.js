fc3eca1a503da78c4d5f6babaf7ae441efe76bde
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-08-04 16:32:25 +0330

    repository updated

--- JS Course/13 - Advanced DOM and Events/main.js
+++ JS Course/13 - Advanced DOM and Events/main.js
@@ -5,3 +5,12 @@ const p = document.querySelectorAll("p");
 console.log(p);
 // can use forEach on nodelists
 p.forEach((e, i, arr) => console.log(e));
+
+// Selecting, Creating, and Deleting Elements
+const header = document.querySelector("body");
+console.log(header);
+
+const message = document.createElement("p");
+message.classList.add("para");
+message.innerHTML = `hello <button>click</button> `;
+header.prepend(message);
