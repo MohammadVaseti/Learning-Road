6db39504924de98016b417d61334feac4897f021
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-06 11:06:13 +0330

    repository updated

--- Api/app.js
+++ Api/app.js
@@ -1,7 +1,20 @@
 "use strict";
 
 // "https://jsonplaceholder.typicode.com/posts"
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-  .then((response) => response.json())
-  .then((data) => console.log(data.id))
-  .catch((err) => console.log("err", err));
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data.id))
+//   .catch((err) => console.log("err", err));
+
+// with async
+
+async function getUsers(params) {
+  try {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/posts/3");
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+getUsers();
