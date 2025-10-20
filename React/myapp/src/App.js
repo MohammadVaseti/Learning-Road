9c2a7cf922e37443aae1fa3893014ae84531170f
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-10-20 11:16:16 +0330

    repo updated

--- React/myapp/src/App.js
+++ React/myapp/src/App.js
@@ -2,8 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  function username(user) {
+    return user.f + " " + user.l;
+  }
+  const userObj = {
+    f: "mohammad",
+    l: "vaseti",
+  };
+  const name = "mohammad";
   const element = <h1>hi there</h1>;
-  return <h6>{element}</h6>;
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{element}</div>
+      <div>{username(userObj)}</div>
+    </div>
+  );
 }
 
 export default App;
