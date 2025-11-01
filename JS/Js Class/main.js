bdb4472c3475377b1faabdb2da907fcd0eecffbe
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-11-01 11:06:26 +0330

    repo updated

--- JS/Js Class/main.js
+++ JS/Js Class/main.js
@@ -7,3 +7,13 @@ class Car {
 }
 
 const newCar = new Car("ford");
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const user1 = new User("mohammad", 27);
+console.log(user1.name, user1.age);
