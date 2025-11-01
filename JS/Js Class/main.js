c3c83d3081477998713dd6796ab8971a779fac27
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-11-01 11:11:35 +0330

    repo updated

--- JS/Js Class/main.js
+++ JS/Js Class/main.js
@@ -17,3 +17,12 @@ class User {
 
 const user1 = new User("mohammad", 27);
 console.log(user1.name, user1.age);
+
+class User2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+const obj = new User2("ali", 28);
+console.log(obj.name, obj.age);
