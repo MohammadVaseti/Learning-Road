14e69708e47a22e0a9ab3b32a28e0eb02d1eba12
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-11-05 12:12:29 +0330

    repo updated

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -4,9 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const element = <h1>hello react</h1>;
+  function formatUser(user) {
+    return user.firstName + " " + user.lastName;
+  }
 
-  return <>{element}</>;
+  function greeting(user) {
+    if (user) {
+      return <h1>hello {formatUser(user)}</h1>;
+    } else {
+      return <p>hello stranger</p>;
+    }
+  }
+
+  const user = {
+    firstName: "mohammad",
+    lastName: "vaseti",
+  };
+  return <div> {greeting(user)}</div>;
 }
 
 export default App;
