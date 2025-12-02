e3b4baab654823c04f651e693337992493211855
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-12-02 15:38:32 +0330

    repo updated

--- React/net-ninja-crash/src/home.jsx
+++ React/net-ninja-crash/src/home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
+  const handleClick = () => {};
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button className="btn">Click me</button>
     </div>
   );
 };
