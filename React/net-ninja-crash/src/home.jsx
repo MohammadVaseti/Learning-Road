5a383634c67e9f5507a0bd0a058141fe91989c65
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-12-22 14:51:15 +0330

    repo updated

--- React/net-ninja-crash/src/home.jsx
+++ React/net-ninja-crash/src/home.jsx
@@ -2,56 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My New Websie",
-      body: " lorem ipsom ...",
-      id: 1,
-      author: " Mohammad",
-    },
-    {
-      title: "Welcome Party",
-      body: " lorem ipsom ...",
-      id: 2,
-      author: "Ali",
-    },
-    {
-      title: "Top Tips To Be a Developer",
-      body: " lorem ipsom ...",
-      id: 3,
-      author: "Hossein",
-    },
-    {
-      title: "Top Tips To Be a Developer",
-      body: " lorem ipsom ...",
-      id: 4,
-      author: "Hossein",
-    },
-  ]);
-
-  const [name, setName] = useState("mohammad");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect ran");
-    console.log(name);
-  }, [name]);
+    fetch(" http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="all blogs !" handleDelete={handleDelete} />
-      <button
-        onClick={() => {
-          setName("mohsen");
-        }}
-      >
-        Chane Name
-      </button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="all blogs !" />}
     </div>
   );
 };
