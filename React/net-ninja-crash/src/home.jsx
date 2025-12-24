c30f4e0c5afc8a2fd764eaca571b4a1fd79f57e2
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-12-24 14:53:30 +0330

    repo updated

--- React/net-ninja-crash/src/home.jsx
+++ React/net-ninja-crash/src/home.jsx
@@ -6,17 +6,21 @@ const Home = () => {
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch(" http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 2000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>loading ... </div>}
       {blogs && <BlogList blogs={blogs} title="all blogs !" />}
     </div>
   );
