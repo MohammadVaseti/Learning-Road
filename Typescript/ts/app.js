d95ad591ba3e08a122fadd71313c15ffb32a15fc
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-01-03 11:47:23 +0330

    repo updated

--- Typescript/ts/app.js
+++
@@ -1,5 +0,0 @@
-var age = 30;
-console.log(age);
-var fName = "mohammad";
-var bool = true;
-fName = " luigi";
--- Typescript/ts/app.ts
+++
@@ -1,5 +0,0 @@
-let age: number = 30;
-console.log(age);
-let fName: string = "mohammad";
-let bool: boolean = true;
-fName = " luigi";
