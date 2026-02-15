5a66645b3e539224dd19f6219ff75c6eb998bd4b
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-15 11:44:22 +0330

    repo updated

--- React-Typescript/React-TS/my-app/src/App.tsx
+++ React-Typescript/React-TS/my-app/src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  let fName: string = "mohammad";
+  console.log(fName);
+
+  let counter: number = 27;
+
+  console.log(counter);
+
+  return (
+    <>
+      <h1>Typescript</h1>
+    </>
+  );
 }
 
 export default App;
