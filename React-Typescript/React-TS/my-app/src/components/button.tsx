dfc7a0819fa36341145828b085a242028feeec7e
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-16 14:47:50 +0330

    repo updated

--- React-Typescript/React-TS/my-app/src/components/button.tsx
+++ React-Typescript/React-TS/my-app/src/components/button.tsx
@@ -1,4 +1,8 @@
-const Button = () => {
+import { ComponentProps } from "react";
+
+type Button = React.ComponentProps<"button">;
+
+const Button = ({}: Button) => {
   return (
     <>
       <button>click me</button>
