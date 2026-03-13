9210e2dcb2067b202166419241f985e606b85e5f
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-03-13 10:56:30 +0330

    repo updated

--- React-Typescript/React-TS/my-app/src/App.tsx
+++ React-Typescript/React-TS/my-app/src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ React-Typescript/React-TS/my-app/src/components/hello-world.tsx
@@ -0,0 +1,16 @@
+type HelloworldProps = {
+  name: string;
+  color: string;
+};
+
+const HelloWorld = (props: HelloworldProps) => {
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default HelloWorld;
