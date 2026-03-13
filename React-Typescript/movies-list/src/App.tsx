e9bac325c775d4c1a2dc57703fccb6ce0505956f
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-03-13 11:31:10 +0330

    repo updated

--- React-Typescript/movies-list/src/App.tsx
+++ React-Typescript/movies-list/src/App.tsx
@@ -9,18 +9,16 @@ import Movie6Picture from "../public/assets/img/dark.jpg";
 import { MovieProps } from "./components/render-movies/components/movie";
 
 function App() {
-    const movies : MovieProps[] = [
-        { id: 1, rating: 4.5, title: "Hannibal", picture: Movie1Picture },
-        { id: 2, rating: 4.2, title: "Breaking Bad", picture: Movie2Picture },
-        { id: 3, rating: 3.7, title: "Band Of Brothers", picture: Movie3Picture },
-        { id: 4, rating: 4.9, title: "Chernobyl", picture: Movie4Picture },
-        { id: 5, rating: 4.4, title: "The Sopranos", picture: Movie5Picture },
-        { id: 6, rating: 4.9, title: "Dark", picture: Movie6Picture },
-    ];
+  const movies: MovieProps[] = [
+    { id: 1, rating: 4.5, title: "Hannibal", picture: Movie1Picture },
+    { id: 2, rating: 4.2, title: "Breaking Bad", picture: Movie2Picture },
+    { id: 3, rating: 3.7, title: "Band Of Brothers", picture: Movie3Picture },
+    { id: 4, rating: 4.9, title: "Chernobyl", picture: Movie4Picture },
+    { id: 5, rating: 4.4, title: "The Sopranos", picture: Movie5Picture },
+    { id: 6, rating: 4.9, title: "Dark", picture: Movie6Picture },
+  ];
 
-    return (
-        <RenderMovies movies={movies}/>
-    );
+  return <RenderMovies movies={movies} />;
 }
 
 export default App;
--- React-Typescript/movies-list/src/components/render-movies/components/movie/components/title.tsx
+++ React-Typescript/movies-list/src/components/render-movies/components/movie/components/title.tsx
@@ -1,9 +1,9 @@
 export type TitleProps = {
-    children : React.ReactNode;
-}
+  children: string;
+};
 
-const Title = ({children} : TitleProps) => {
-    return <h2>{children}</h2>;
+const Title = ({ children }: TitleProps) => {
+  return <h2>{children}</h2>;
 };
 
 export default Title;
