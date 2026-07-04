31ef5797e09ff3a86194f915c18acf7fb21b14e3
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-07-04 10:35:05 +0330

    repo updated

--- Redux/project/myProject/src/redux/counterSlice.js
+++ Redux/project/myProject/src/redux/counterSlice.js
@@ -1,4 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
 
-
-
-export const counterSlice =
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+});
