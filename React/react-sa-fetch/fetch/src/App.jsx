9a236f992f0ddf1eae70306a7cab81aed454c6c6
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-08-15 11:12:46 +0330

    repo updated

--- React/react-sa-fetch/fetch/src/App.jsx
+++ React/react-sa-fetch/fetch/src/App.jsx
@@ -1,20 +1,23 @@
 import axios, { Axios } from "axios";
+import { useState } from "react";
 
 function App() {
   // fetch("https://catfact.ninja/fact")
   //   .then((res) => res.json())
   //   .then((data) => console.log(data));
 
+  const [catFact, setCatFact] = useState("");
+
   const getFetch = () => {
     axios
       .get("https://catfact.ninja/fact")
-      .then((res) => console.log(res.data.fact));
+      .then((res) => setCatFact(res.data.fact));
   };
 
   return (
     <>
       <button onClick={getFetch}>Fetch</button>
-      <p></p>
+      <p>{catFact}</p>
     </>
   );
 }
