873e284534a890c765ea951c394e3d5ce1212d79
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-08-17 09:53:29 +0330

    repo updated

--- React/react-sa-fetch/fetch/src/Age.jsx
+++ React/react-sa-fetch/fetch/src/Age.jsx
@@ -3,9 +3,13 @@ import { useState } from "react";
 const Age = () => {
   const [name, setName] = useState("");
 
+  const nameHolder = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <>
-      <input type="text" placeholder="Enter Your Name" />
+      <input type="text" placeholder="Enter Your Name" onChange={nameHolder} />
       <button>Predict</button>
       <h1></h1>
     </>
